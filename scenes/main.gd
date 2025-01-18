e094c0f8ba808b2fb1c4ee3df69f7276772c8d63
Timothy Cope <kirbycope@gmail.com>
2025-01-18 11:14:27 -0800

    config

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 ## Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+
 	# Disable the mouse pointer and capture the motion
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
